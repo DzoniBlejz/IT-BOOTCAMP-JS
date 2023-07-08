34bed9136ea87a96cf43c048e2532980357b6eed
DzoniBlejz <alm_nikola@hotmail.com>
2023-07-08 16:43:36 +0200

    dzejlanov zadatak

--- zadatak18/script.js
+++ zadatak18/script.js
@@ -1,26 +1,26 @@
-let array = [
-	"apple",
-	"banana",
-	"strawberry",
-	["banana", "apple", "strawberry"],
-];
+// let array = [
+// 	"apple",
+// 	"banana",
+// 	"strawberry",
+// 	["banana", "apple", "strawberry"],
+// ];
 
-let brojac = 0;
+// let brojac = 0;
 
-for (let i = 0; i < array.length; i++) {
-	if (array[i] === "apple") {
-		brojac++;
-	}
-	if (Array.isArray(array[i])) {
-		for (let j = 0; j < array[i].length; j++) {
-			if (array[j] === "apple") {
-				brojac++;
-				console.log("nasao sam jabuku u drugi niz");
-			}
-		}
-	}
-}
-console.log(brojac);
+// for (let i = 0; i < array.length; i++) {
+// 	if (array[i] === "apple") {
+// 		brojac++;
+// 	}
+// 	if (Array.isArray(array[i])) {
+// 		for (let j = 0; j < array[i].length; j++) {
+// 			if (array[j] === "apple") {
+// 				brojac++;
+// 				console.log("nasao sam jabuku u drugi niz");
+// 			}
+// 		}
+// 	}
+// }
+// console.log(brojac);
 
 // let spojeniNiz = array.flat();
 // let brojac = 0;
@@ -32,3 +32,27 @@ console.log(brojac);
 // }
 
 // console.log(brojac);
+
+// dzejlanov zadatak:
+
+niz1 = [
+	"nesto",
+	null,
+	"drugo",
+	false,
+	5,
+	3,
+	10,
+	true,
+	undefined,
+	undefined,
+	"plastika",
+	105,
+	103.2,
+	null,
+	"dva" == "dva",
+	1001,
+	[5, 5, 3],
+	{ ime: "dzelal", godine: "koliko ti hoces" },
+	{ ime: "nikola", godine: 26 },
+];
