093d7687a5b716a4f3500a776c08b8bfccdffa0c
DzoniBlejz <alm_nikola@hotmail.com>
2023-07-18 16:26:49 +0200

    e

---
+++ zadatak20/script.js
@@ -0,0 +1,39 @@
+// function rollDice() {
+// 	let roll = Math.floor(Math.random() * 6) + 1;
+// 	console.log(roll);
+// }
+
+// function roll(times) {
+// 	for (let i = 0; i < times; i++) {
+// 		rollDice();
+// 	}
+// 	console.log("pozvao sam roll funkciju");
+// }
+
+// roll(10);
+
+let niz = [];
+function rollThousand() {
+	let roll = Math.floor(Math.random() * 1000);
+	return roll;
+}
+
+function pozivanje(array, number) {
+	for (let i = 0; i < number; i++) {
+		array.push(rollThousand());
+	}
+}
+
+pozivanje(niz, 10);
+console.log(niz);
+
+const noviNiz = [56, 412, 30, 312, 3, 4, 5, 6, 8, 123];
+
+// funkcija koja nalazi srednju vrednost noviNiz niza.
+// funkcija koja nalazi najmanju vrednost u nizu
+// funckija koja nalazi najvecu vrednost u nizu
+// funkcija koja sabira sve elemente niza
+// funkcija koja sabira samo parne elemente niza
+// funkcija koja sabira samo neparne elemente niza
+// funkcija koja kvadrira svaku vrednost niza i upisuje je u novi
+// niz i ispisuje oba niza
