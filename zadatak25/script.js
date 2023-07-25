5c222d82fe49ed622e78d795c52a196011f1f22b
DzoniBlejz <alm_nikola@hotmail.com>
2023-07-25 20:26:03 +0200

    meessagee

---
+++ zadatak25/script.js
@@ -0,0 +1,11 @@
+//Zadatak 4: Pronalaženje prvog pozitivnog broja
+//Napišite funkciju koja pronalazi i vraća prvi pozitivan broj u nizu. Ako nema pozitivnih brojeva, funkcija treba vratiti null.
+
+// Zadatak 3: Every
+// Napišite funkciju koja provjerava jesu li svi elementi niza veći od 10.
+
+// Zadatak 2: Some
+// Napišite funkciju koja provjerava je li barem jedan od elemenata u nizu parni broj.
+
+// Zadatak 1: Filter zadatak
+// Napišite funkciju koja prima niz brojeva i vraća novi niz koji sadrži samo pozitivne brojeve.
